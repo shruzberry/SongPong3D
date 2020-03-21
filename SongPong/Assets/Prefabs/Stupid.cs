9e94b3320bda294343744605a53aaac952428656
Sean <shruzberry14@gmail.com>
2020-03-21 10:23:07 -0500

    Revert "Revert "Revert "added surfer"""
    This reverts commit 5ee941419c36780192ebc0deef150c085da460fd.

--- SongPong/Assets/Prefabs/Stupid.cs
+++ SongPong/Assets/Prefabs/Stupid.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Dumb : MonoBehaviour
+public class Dumb1 : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
