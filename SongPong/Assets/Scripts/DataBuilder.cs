cc688843dc0c825cc9a8fe52efbd7028b3335cc7
Sean <shruzberry14@gmail.com>
2020-03-22 11:43:35 -0500

    partway through click to add placeeholder

---
+++ SongPong/Assets/Scripts/DataBuilder.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.IO;
+
+public class DataBuilder : MonoBehaviour
+{
+    public string fileName;
+    public float bpm;
+
+    private string path;
+    private StreamWriter writer;
+
+    void Awake()
+    {
+        path = "Assets/Resources/SongData/" + fileName + ".csv";
+        writer = new StreamWriter(path, true);
+    }
+
+    void AddPlaceholder()
+    {
+        writer.WriteLine("null, ");
+    }
+}
