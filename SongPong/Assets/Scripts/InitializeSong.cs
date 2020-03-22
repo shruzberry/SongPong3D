7d283e4d25dad67d43a0a0523b58131e830d1d60
Sean Hruz <shruzberry14@gmail.com>
2020-03-21 20:38:01 -0500

    initialized song loader

---
+++ SongPong/Assets/Scripts/InitializeSong.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InitializeSong : MonoBehaviour
+{
+    // TODO: create a function similar to the slider function to...
+	// 1) Load song by name onto the audio player
+	// 2) Locate the correct song data file for reading
+	// 3) Create Ball prefabs from file data
+	// 4) Set slider back to start of song
+}
