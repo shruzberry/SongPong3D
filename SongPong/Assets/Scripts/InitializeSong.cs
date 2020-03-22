e72f3012cb3740fbdde401d8b10b1fd2d845993e
Sean <shruzberry14@gmail.com>
2020-03-22 15:27:57 -0500

    updated song init todo

--- SongPong/Assets/Scripts/InitializeSong.cs
+++ SongPong/Assets/Scripts/InitializeSong.cs
@@ -5,9 +5,12 @@ using UnityEngine.UI;
 
 public class InitializeSong : MonoBehaviour
 {
-    // TODO: create a function similar to the slider function to...
+  // TODO: create a function similar to the slider function to...
 	// 1) Load song by name onto the audio player
-	// 2) Locate the correct song data file for reading
+	// 2) Locate the correct song data file for reading (.tsv)
+  //    - songName_notes.tsv (for the notes)
+  //    - songName_data.dat (for bpm, deelay, and style)
+  //    - Put all in folder named songName
 	// 3) Create Ball prefabs from file data
 	// 4) Set slider back to start of song
 }
