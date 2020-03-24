c9c622ba079a598bc5e97511577ca5058c8f317a
Sean <shruzberry14@gmail.com>
2020-03-24 10:17:48 -0500

    added UI preview to note builder

---
+++ SongPong/Assets/Scripts/UIFollowMouse.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIFollowMouse : MonoBehaviour
+{
+    public float offsetX = 0;
+    public float offsetY = 0;
+
+    void Update()
+    {
+        this.transform.position = new Vector3 (Input.mousePosition.x, Input.mousePosition.y, 0);
+    }
+}
