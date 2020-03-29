6cc40947654169d3dde9df8837779501710a0b30
Sean <shruzberry14@gmail.com>
2020-03-29 18:12:28 -0500

    added snap to paddle option

--- SongPong/Assets/Scripts/UIFollowMouse.cs
+++ SongPong/Assets/Scripts/UIFollowMouse.cs
@@ -6,9 +6,19 @@ public class UIFollowMouse : MonoBehaviour
 {
     public float offsetX = 0;
     public float offsetY = 0;
+    public bool snapToPaddle = false;
+
+    private float paddleHeight = 110;
 
     void Update()
     {
-        this.transform.position = new Vector3 (Input.mousePosition.x, Input.mousePosition.y, 0);
+        if (snapToPaddle)
+        {
+            this.transform.position = new Vector3 (Input.mousePosition.x + offsetX, paddleHeight + offsetY, 0);
+        }
+        else
+        {
+            this.transform.position = new Vector3 (Input.mousePosition.x + offsetX, Input.mousePosition.y + offsetY, 0);
+        }
     }
 }
