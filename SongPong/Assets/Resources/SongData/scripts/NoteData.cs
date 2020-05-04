346dd330bb9b2b2feb6ac7c8b9e8958b4ab962bb
Sean <shruzberry14@gmail.com>
2020-05-04 11:06:19 -0500

    added data scripts for ball and note

---
+++ SongPong/Assets/Resources/SongData/scripts/NoteData.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenuAttribute(fileName="note", menuName="Song Data/Note")]
+public class NoteData : ScriptableObject
+{
+    public Vector2 hitPosition;
+    public float hitTime;
+}
