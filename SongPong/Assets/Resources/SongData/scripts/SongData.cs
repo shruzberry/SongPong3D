5abc6345024b305314cdc689dc38a03ad293fe0b
Sean <shruzberry14@gmail.com>
2020-05-07 11:24:27 -0500

    rebuilt song controller

---
+++ SongPong/Assets/Resources/SongData/scripts/SongData.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenuAttribute(fileName="Song", menuName="Song Data/Song")]
+public class SongData : ScriptableObject
+{
+    public int bpm;
+    public string name;
+    public AudioClip song;
+    public float startTime = 0.0f;
+}
