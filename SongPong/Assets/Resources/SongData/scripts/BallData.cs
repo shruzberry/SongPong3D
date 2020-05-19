119e3b2aafd24389dcafec7c335be8fff42c8a48
Sean <shruzberry14@gmail.com>
2020-05-19 14:49:34 -0400

    working

--- SongPong/Assets/Resources/SongData/scripts/BallData.cs
+++ SongPong/Assets/Resources/SongData/scripts/BallData.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEditor;
 using Types;
 
 [CreateAssetMenuAttribute(fileName="Ball", menuName="Song Data/Ball")]
@@ -10,4 +11,20 @@ public class BallData : ScriptableObject
     public BallTypes type;
     public NoteData[] notes;
     public GameObject prefab;
+
+    public void OnEnable()
+    {
+        prefab = Resources.Load("Prefabs/SimpleBall") as GameObject;
+        type = BallTypes.simple;
+        notes = new NoteData[1];
+        notes[0] = (NoteData) ScriptableObject.CreateInstance(typeof(NoteData));
+    }
+
+    void SaveData()
+    {
+        var note = Editor.CreateInstance<NoteData>();
+        AssetDatabase.CreateAsset(note, "Assets/Resources/SongData/paradise/Notes/");
+    }
 }
+
+
