6dfa55f603bd4b12cff3cec8b4ac2bc28f0b0504
Sean <shruzberry14@gmail.com>
2020-05-20 15:36:49 -0400

    Sean Goofing up and fixing manually

--- SongPong/Assets/Resources/SongData/scripts/SongData.cs
+++ SongPong/Assets/Resources/SongData/scripts/SongData.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEditor;
 
 [CreateAssetMenuAttribute(fileName="Song", menuName="Song")]
 public class SongData : ScriptableObject
@@ -8,7 +9,27 @@ public class SongData : ScriptableObject
     public float currentTime;
     public int bpm;
     public string name;
+    public string dataPath;
     public AudioClip song;
     public int startBeat = 0;
     public int endBeat = 0;
+
+    public string myPath;
+
+    public void OnEnable()
+    {
+        myPath = AssetDatabase.GetAssetPath(this);
+        dataPath = GetDataPath(myPath);
+
+    }
+
+    private string GetDataPath(string myPath)
+    {
+        string str;
+        
+        int lastSlashPos = myPath.LastIndexOf("/");
+        str = myPath.Substring(0, lastSlashPos);
+
+        return str;
+    }
 }
---
+++ SongPong/Assets/Scripts/MenuLoader.cs
@@ -0,0 +1,85 @@
+﻿/*+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=
+________ DEFENITION ________
+Class Name: MenuLoader.cs
+Purpose: Loads all songs in SongData Foler into the select menu as buttons
+
+________ USAGE ________
+* Attach to GameObject
+* Connect it to the UI Menu Object
+* Call .LoadAllSongs();
+
+________ ATTRIBUTES ________
+
+
+________ FUNCTIONS ________
++ LoadAllSongs();
+
++=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=*/
+
+/*+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=
+* DEPENDENCIES
+*+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=*/
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.IO;
+
+public class MenuLoader : MonoBehaviour
+{
+/*+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=
+* MEMBERS
+*+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=*/
+
+    public List<SongData> songs;
+    public Button buttonPrefab;
+    public float verticalOffset;
+    public int spacing;
+
+    private Vector2 position;
+
+/*+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=
+* PUBLIC FUNCTIONS
+*+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=*/
+
+/*+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=
+* RUNTIME FUNCTIONS
+*+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=*/
+
+    public void Start()
+    {
+        position = new Vector2(0.0f, verticalOffset);
+        loadAllSongs();
+    }
+
+/*+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=
+* PRIVATE FUNCTIONS
+*+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=*/
+
+    private void loadAllSongs()
+    {
+        foreach(SongData song in songs)
+        {
+            AddButton(buttonPrefab, song.name);
+            print("laoding song: " + song.name);
+        }
+    }
+
+    private void AddButton(Button prefab, string name)
+    {
+        Button button = Instantiate(prefab);
+        button.name = name;
+        //button.onClick.AddListener(callback);
+ 
+        Text text = button.GetComponentInChildren<Text>();
+        if (text)
+            text.text = name;
+ 
+        RectTransform transform = button.GetComponent<RectTransform>();
+        transform.SetParent(GameObject.Find("Menu").transform, false);
+        transform.anchoredPosition = position;
+        position = position + (Vector2.down * spacing);
+    }
+}
+
