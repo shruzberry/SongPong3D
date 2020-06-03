9879363f25c9ba31868a7faad940f77ea5b44171
Sean <shruzberry14@gmail.com>
2020-06-03 11:28:48 -0400

    Rebuilt the control thing

---
+++ SongPong/Assets/Scripts/Paddles/PaddleMover.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PaddleMover : MonoBehaviour
+{
+    public float speed = 10.0f;
+    public int playerIndex = 0;
+
+    private Vector2 _movement;
+    private Vector2 paddlePos;
+
+    public void SetInputVector(Vector2 direction)
+    {
+        _movement = direction;
+    }
+
+    void Update()
+    {
+        paddlePos += _movement * speed * Time.deltaTime;
+        transform.position = paddlePos;
+    }
+}
