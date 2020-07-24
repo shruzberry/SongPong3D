fc06a12ae79209c364e8b9033e98dfaca7814992
Sean <shruzberry14@gmail.com>
2020-07-24 11:28:19 -0500

    need a break

---
+++ SongPong/Assets/Scripts/Tools/beat.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class beat : MonoBehaviour
+{
+    private TextMeshProUGUI beatNum;
+    private SongController sc;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        beatNum = GetComponent<TextMeshProUGUI>();
+        sc = GameObject.Find("SongController").GetComponent<SongController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        beatNum.text = "" + Mathf.Round(sc.GetSongTimeBeats());
+    }
+}
