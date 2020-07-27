3f2154debb71f8c15ac0cab4ef7145fea98ffa8d
Sean <shruzberry14@gmail.com>
2020-07-27 09:38:29 -0500

    check

--- SongPong/Assets/Editor/SongBuilder.cs
+++ SongPong/Assets/Editor/SongBuilder.cs
@@ -287,7 +287,7 @@ public class SongBuilder : EditorWindow
         //Ball Field
         foreach(BallData ball in ballList)
         {
-            if((ball.notes[0].hitBeat > 800) || ball.notes[0].hitBeat < 1500)
+            if((ball.notes[0].hitBeat > 900) || (ball.notes[0].hitBeat <= 200))
             {
             
             numBallsDisplayed += 1;
