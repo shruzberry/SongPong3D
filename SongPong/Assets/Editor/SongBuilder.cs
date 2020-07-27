6a319ec9fea2c4771a535eaefa33860a84713a07
Sean <shruzberry14@gmail.com>
2020-07-27 09:21:28 -0500

    quick save

--- SongPong/Assets/Editor/SongBuilder.cs
+++ SongPong/Assets/Editor/SongBuilder.cs
@@ -287,7 +287,7 @@ public class SongBuilder : EditorWindow
         //Ball Field
         foreach(BallData ball in ballList)
         {
-            if((ball.notes[0].hitBeat >= jumpToTime && numBallsDisplayed < 50) || ball.notes[0].hitBeat < 100)
+            if((ball.notes[0].hitBeat > 800) || ball.notes[0].hitBeat < 1500)
             {
             
             numBallsDisplayed += 1;
